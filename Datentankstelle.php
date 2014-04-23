677ef1c5721f005948a89d73882fa18780bc862e
Kai Nissen <kai.nissen@wikimedia.de>
2014-04-23 18:10:34 +0200

    reintroducing start page

--- Datentankstelle.php
+++ Datentankstelle.php
@@ -64,8 +64,9 @@ class Datentankstelle {
 			default:
 				include( "templates/" . $_SESSION["skin"] . "/_head.tpl.phtml" );
 				# show template of main entry point
-				$this->_subject = "Hauptkategorie";
-				new CategoryInfo( $this->_subject );
+				//$this->_subject = "Hauptkategorie";
+				//new CategoryInfo( $this->_subject );
+				include( "templates/" . $_SESSION["skin"] . "/start.tpl.phtml" );
 				include( "templates/" . $_SESSION["skin"] . "/_foot.tpl.phtml" );
 				break;
 		}
