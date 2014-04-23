11efeeccea7786a03162ef6873db222cc202afb7
Kai Nissen <kai.nissen@wikimedia.de>
2014-04-23 14:49:18 +0200

    path to data sets

--- local-config-sample.php
+++ local-config-sample.php
@@ -4,3 +4,4 @@ define( "WIKI_API_URL", "http://localhost/mediawiki/api.php" );
 define( "TITLE_CATEGORIES", "Datenkategorie" );
 define( "TITLE_SETS", "Datensatz" );
 define( "USB_MOUNT_DIR" , "/media/datentankstelle/" );
+define( "DOWNLOAD_FOLDER", "/var/www/downloads/data-sets/" );
